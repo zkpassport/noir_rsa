b0b813b740fd334de36cdc46d660b5c758b721a0
github-action-benchmark <github@users.noreply.github.com>
2026-09-11 09:17:44 +0000

    add Brillig Bytecode Size (customSmallerIsBetter) benchmark result for 900e361e8c6bf7761e0a69ee65067b3274790d03

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789118260316,
+  "lastUpdate": 1789118264235,
   "repoUrl": "https://github.com/zkpassport/noir_rsa",
   "entries": {
     "ACIR Opcodes": [
@@ -60,6 +60,37 @@ window.BENCHMARK_DATA = {
             "name": "test_verify_sha256_pkcs1v15_2048.json/main",
             "value": 16350,
             "unit": "circuit_size"
+          }
+        ]
+      }
+    ],
+    "Brillig Bytecode Size": [
+      {
+        "commit": {
+          "author": {
+            "email": "saleel@saleel.xyz",
+            "name": "saleel",
+            "username": "saleel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "900e361e8c6bf7761e0a69ee65067b3274790d03",
+          "message": "Merge pull request #12 from zkpassport/as/bump-workflows\n\nchore: Bump the workflows",
+          "timestamp": "2026-09-11T13:17:13+04:00",
+          "tree_id": "8b4f91577c8146eaa67d6e2fc6fc2608f5e1302d",
+          "url": "https://github.com/zkpassport/noir_rsa/commit/900e361e8c6bf7761e0a69ee65067b3274790d03"
+        },
+        "date": 1789118264221,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_verify_sha256_pkcs1v15_2048",
+            "value": 2451,
+            "unit": "opcodes"
           }
         ]
       }
