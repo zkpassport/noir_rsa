71c3b2dbeb31c68d8e2d4d59a5f4cf540f09385c
github-action-benchmark <github@users.noreply.github.com>
2026-09-14 11:20:56 +0000

    add Circuit Size (customSmallerIsBetter) benchmark result for 516db18a732d3426d9417444e5b6c0733d2b0d1c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789384854085,
+  "lastUpdate": 1789384856301,
   "repoUrl": "https://github.com/zkpassport/noir_rsa",
   "entries": {
     "ACIR Opcodes": [
@@ -761,6 +761,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "verify_sha256_pss_2048_const.json/main",
             "value": 58252,
+            "unit": "circuit_size"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "75146596+Sarkoxed@users.noreply.github.com",
+            "name": "Sarkoxed",
+            "username": "Sarkoxed"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "516db18a732d3426d9417444e5b6c0733d2b0d1c",
+          "message": "Merge pull request #13 from zkpassport/as/readme-update\n\nchore: Update README, expose a backward-compatible public API, and bump noir-bignum",
+          "timestamp": "2026-09-14T14:20:25+03:00",
+          "tree_id": "1af3b43cfdb4e53126219e6ad6d5b4def25f2ed8",
+          "url": "https://github.com/zkpassport/noir_rsa/commit/516db18a732d3426d9417444e5b6c0733d2b0d1c"
+        },
+        "date": 1789384856287,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "exponentiate.json/main",
+            "value": 51194,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "exponentiate_const.json/main",
+            "value": 19526,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_sha256_pkcs1v15_2048.json/main",
+            "value": 51945,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_sha256_pkcs1v15_2048_const.json/main",
+            "value": 20240,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_sha256_pss_2048.json/main",
+            "value": 90110,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_sha256_pss_2048_const.json/main",
+            "value": 58412,
             "unit": "circuit_size"
           }
         ]
