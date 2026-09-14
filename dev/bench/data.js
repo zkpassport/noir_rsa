76e68c0f4ccaf8e9d9a9d5847416159c4cc6f870
github-action-benchmark <github@users.noreply.github.com>
2026-09-14 08:55:22 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for 3a8b14a322ab6d3772850cce2a98583f5aaf2184

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789376000447,
+  "lastUpdate": 1789376122404,
   "repoUrl": "https://github.com/zkpassport/noir_rsa",
   "entries": {
     "ACIR Opcodes": [
@@ -106,6 +106,60 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/zkpassport/noir_rsa/commit/9adeac2ad1779f0958c1880a443bb33e71fc826a"
         },
         "date": 1789375995278,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "exponentiate.json/main",
+            "value": 13625,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "exponentiate_const.json/main",
+            "value": 2702,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_sha256_pkcs1v15_2048.json/main",
+            "value": 14131,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_sha256_pkcs1v15_2048_const.json/main",
+            "value": 3208,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_sha256_pss_2048.json/main",
+            "value": 14960,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_sha256_pss_2048_const.json/main",
+            "value": 4037,
+            "unit": "acir_opcodes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "75146596+Sarkoxed@users.noreply.github.com",
+            "name": "Sarkoxed",
+            "username": "Sarkoxed"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3a8b14a322ab6d3772850cce2a98583f5aaf2184",
+          "message": "Merge pull request #15 from zkpassport/as/mgf1-refactoring\n\nchore: generalize mgf1 over the hash function",
+          "timestamp": "2026-09-14T11:54:48+03:00",
+          "tree_id": "7e71262ccf7da349d570083d3d3316a4918407a5",
+          "url": "https://github.com/zkpassport/noir_rsa/commit/3a8b14a322ab6d3772850cce2a98583f5aaf2184"
+        },
+        "date": 1789376120612,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
